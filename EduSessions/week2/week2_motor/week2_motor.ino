9c15cd0a5df3a2ba769b60e1d550bf47604195cc
piratecat-lover <johngalt827@snu.ac.kr>
2024-04-29 18:50:40 +0900

    Completed Arduino test

--- EduSessions/week2/week2_motor/week2_motor.ino
+++ EduSessions/week2/week2_motor/week2_motor.ino
@@ -1,9 +1,10 @@
-const int motor1pin1 = 3;
-const int motor1pin2 = 11;
-const int motor2pin1 = 5;
-const int motor2pin2 = 6;
-const byte irPin1 = A0;
-const byte irPin2 = A1;
+const int motor1pin1 = 3; // Backward left
+const int motor1pin2 = 11; // Forward left
+const int motor2pin1 = 5; // Forward right
+const int motor2pin2 = 6; // Backward right
+const byte irPin1 = A1; // left
+const byte irPin2 = A0; // right
+const int thr=1.05;
 
 void setup() {
   Serial.begin(115200);
@@ -17,43 +18,31 @@ void setup() {
 }
 
 void forward() {
-  analogWrite(motor1pin1, 255);
-  analogWrite(motor2pin1, 255);
-  analogWrite(motor1pin2, 0);
-  analogWrite(motor2pin2, 0);
-  delay(2000);
-}
-
-void backward() {
   analogWrite(motor1pin1, 0);
-  analogWrite(motor2pin1, 0);
-  analogWrite(motor1pin2, 255);
-  analogWrite(motor2pin2, 255);
-  delay(2000);
+  analogWrite(motor1pin2, 105);
+  analogWrite(motor2pin1, 155);
+  analogWrite(motor2pin2, 0);
 }
 
 void turn_left() {
-  analogWrite(motor1pin1, 255);
-  analogWrite(motor2pin1, 0);
+  analogWrite(motor1pin1, 0);
   analogWrite(motor1pin2, 0);
-  analogWrite(motor2pin2, 255);
-  delay(2000);
+  analogWrite(motor2pin1, 155);
+  analogWrite(motor2pin2, 0);
 }
 
 void turn_right() {
   analogWrite(motor1pin1, 0);
-  analogWrite(motor2pin1, 255);
-  analogWrite(motor1pin2, 255);
+  analogWrite(motor1pin2, 105);
+  analogWrite(motor2pin1, 0);
   analogWrite(motor2pin2, 0);
-  delay(2000);
 }
 
 void stop() {
   analogWrite(motor1pin1, 0);
-  analogWrite(motor2pin1, 0);
   analogWrite(motor1pin2, 0);
+  analogWrite(motor2pin1, 0);
   analogWrite(motor2pin2, 0);
-  delay(2000);
 }
 
 float InputVoltage(char pinNo, float inputVal) {
@@ -69,18 +58,17 @@ float InputVoltage(char pinNo, float inputVal) {
 }
 
 void loop() {
-  stop();
   float input1 = analogRead(irPin1);
   float input2 = analogRead(irPin2);
   float output1 = InputVoltage(irPin1, input1);
   float output2 = InputVoltage(irPin2, input2);
-  if (output1>2 && output2>2) {
+  if (output1<thr && output2<thr) {
     forward();
-  } else if (output1<2 && output2>2) {
+  } else if (output1>=thr && output2<thr) {
     turn_left();
-  } else if (output1>2 && output2<2) {
+  } else if (output1<thr && output2>=thr) {
     turn_right();
   } else {
-    backward();
+    stop();
   }
 }
